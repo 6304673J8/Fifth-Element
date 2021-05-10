7a1d4680dfa75c9ca2ac381be0c568e63f2389ca
Georghe Julca <petergeorghe.julca@enti.cat>
2021-05-10 11:25:00 +0200

    Prefabs Upgrade

---
+++ Elementum/Assets/Scripts/Scriptable Objects/TileData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+
+public class TileData: MonoBehaviour
+{
+    public TileBase[] tiles;
+
+    public float walkingSpeed, flammable;
+
+}
---
+++ Elementum/Assets/Scripts/TilemapManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    void Update()
+    {
+            
+    }
+}
