27e3745c96d96766f044b858bb0cc05bc6eca75f
Albert <albert.faro@enti.cat>
2021-05-24 20:50:26 +0200

    Prefabs

---
+++ Elementum/Assets/airBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class airBlock : MonoBehaviour
+{
+    public float activating;
+    public float activateDelay = 0.01f;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Fire"))
+        {
+            DestroyObject();
+        }
+    }
+
+    private void DestroyObject()
+    {
+        anim.SetTrigger("isDestroyed");
+        activating += Time.deltaTime;
+        if (activating >= activateDelay)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Elementum/Assets/levelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+     public void Quit()
+    {
+        Debug.Log("Si esto fuera una Build, el juego se habría cerrado.");
+        Application.Quit();
+    }
+    
+}
