206b735f2c4e6c85672d7856963be02c3e56f98d
J0 <petergeorghe.julca@enti.cat>
2021-05-24 19:04:35 +0200

    Tilemap Setup - Character Animator
    WiP Player Movement

---
+++ Elementum/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed;
+    public float jumpSpeed;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkSpeed = 2f;
+        jumpSpeed = 3;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.velocity = new Vector2(walkSpeed, rb.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.velocity = new Vector2(-walkSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+}
